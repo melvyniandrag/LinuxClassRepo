62ac1451fe195fe325ca4b2e8177033fbea5f0a5
melvyniandrag <melvyniandrag@gmail.com>
2021-03-31 08:10:51 -0400

    small week 10 notes modifications. Added nicer whitespace to the Example1 code files.

--- Lectures/Week10_Apache_And_Flask/Example1/ExampleFlask/my_flask_app.py
+++ Lectures/Week10_Apache_And_Flask/Example1/ExampleFlask/my_flask_app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route("/")
 def hello():
     return "I Made Website With  Python + Flask + Linux + Apache2!"
+
 if __name__ == "__main__":
     app.run()
 
